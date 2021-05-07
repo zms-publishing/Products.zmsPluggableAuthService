d2e94c613ad2ceeecef544bec1812cefd490ea82
zmsdev <33760154+zmsdev@users.noreply.github.com>
2021-05-07 11:36:11 +0200

    fixed authentication

--- Products/zmsPluggableAuthService/plugins/ZMSPASSsoPlugin.py
+++ Products/zmsPluggableAuthService/plugins/ZMSPASSsoPlugin.py
@@ -318,7 +318,7 @@ class ZMSPASSsoPlugin(Folder, BasePlugin):
         request = self.REQUEST
         token = request.get(self.header_name, '')
         decoded_token = self.decryptToken(token)
-        if decoded_token:
+        if False and decoded_token:
           user_id = decoded_token['user_id']
           users = getattr(self,'_users',{})
           if user_id in users or users[user_id] != decoded_token:

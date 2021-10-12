f649aa93be3577107ed87cc0284bc2e4ab0db2c0
drfho <dr.frank.hoffmann@web.de>
2021-10-12 16:19:32 +0200

    allow user id attr customisation (reg by DOSIS)

--- Products/zmsPluggableAuthService/plugins/ZMSPASSsoPlugin.py
+++ Products/zmsPluggableAuthService/plugins/ZMSPASSsoPlugin.py
@@ -363,7 +363,8 @@ class ZMSPASSsoPlugin(Folder, BasePlugin):
 
     def get_user_id_attrs(self):
       if not self.hasProperty('user_id_attrs'):
-        self.manage_addProperty('user_id_attrs','onpremisessamaccountname,preferred_username','string')
+        return ['onpremisessamaccountname','preferred_username']
+        # self.manage_addProperty('user_id_attrs','onpremisessamaccountname,preferred_username','string')
       return [ x.strip() for x in (self.user_id_attr).split(',') ]
 
     #
